0eb057bb3c2fe09ea72663155d02aceeb6b6a730
MrAnderson58 <105221921+MrAnderson58@users.noreply.github.com>
2022-06-04 10:55:08 +0300

    hight

--- src/oneclass.java
+++ src/oneclass.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
             Scanner sc = new Scanner(System.in);
             String name = sc.nextLine();
             int old = sc.nextInt();
+            int height = sc.nextInt();
             System.out.println("Hello," + " " + name + "!");
-            System.out.print("Do you" + " " + old + " " + "year?");
+            System.out.println("Do you" + " " + old + " " + "year?");
+            System.out.print("Рост" + "=" + height);
 
 
         }
