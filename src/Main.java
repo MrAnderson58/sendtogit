25d94339391ed77d80d1283e7808fb2306bf9f2b
MrAnderson58 <andrey8261@gmail.com>
2022-06-04 10:33:39 +0300

    commit1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int old = sc.nextInt();
+        System.out.println("Hello," + " " + name + "!");
+        System.out.print("Do you" + " " + old + " " + "year?");
+
+
     }
 }
---
+++ src/oneclass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+    public class oneclass {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String name = sc.nextLine();
+            int old = sc.nextInt();
+            System.out.println("Hello," + " " + name + "!");
+            System.out.print("Do you" + " " + old + " " + "year?");
+
+
+        }
+    }
+
